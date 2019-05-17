6dede1b6a016f6405cca040fcba23fa0e1afc91b
Ania <annalangner06@gmail.com>
2019-05-17 11:39:01 +0200

    Adding the MovieImage class

--- js/script.js
+++ js/script.js
@@ -13,5 +13,38 @@ var Movie = React.createClass ({
 	},
 });
 
+var MovieImage = React.createClass ({
+	propTypes: {
+		src: React.PropTypes.string,
+		alt: React.PropTypes.string,
+	},
+	render: function() {
+		return (React.createElement('img', {src: this.props.movie.src, alt: this.props.movie.title}))
+	}
+});
+
+
+
+var movies = [
+	{
+		id: 1,
+		src: './images/LordOfTheRings.jpg',
+		title: 'The Lord of the Rings',
+		desc: 'Set in the fictional world of Middle-earth, the hobbit Frodo Baggins (Elijah Wood) he and the Fellowship embark on a quest to destroy the One Ring.'
+	},
+	{
+		id: 2,
+		src: './images/GreenMile.jpg',
+		title: 'The Green Mile',
+		desc: 'A retired prison guard tells her friend about an extraordinary man who was sentenced to death for the murder of two 9-year-old girls.'
+	},
+	{
+		id: 3,
+		src: './images/Deadpool.jpg',
+		title: 'Deadpool',
+		desc: 'A former soldier of special forces is subjected to a dangerous experiment. He soon releases his alter ego and starts hunting for a man who almost destroyed his life.'
+	}
+
+];
 
 ReactDOM.render(element, document.getElementById('app'));
